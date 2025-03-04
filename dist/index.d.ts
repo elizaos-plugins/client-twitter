f487807c22fa66cde6fd7a7129a17fc7e4f2da18
Alexander Krustev <krustev.alexandr@elveder.com>
2025-03-04 10:35:21 +0200

    Update dist directory with the new changes

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,6 +4,8 @@ declare const twitterPlugin: {
     name: string;
     description: string;
     clients: _elizaos_core.Client[];
+    providers: _elizaos_core.Provider[];
+    evaluators: _elizaos_core.Evaluator[];
 };
 
 export { twitterPlugin as default };
